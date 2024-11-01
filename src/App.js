e1c6b15119a949c4592addbcb4646f6704a6137b
Marat Kotik <maratkotik97@gmail.com>
2024-11-01 11:39:23 -0400

    fix linting err

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { Box } from "@mantine/core";
 import "./App.scss";
 
-import Header from "./components/Header/Header";
-import MainSection from "./components/MainSection/MainSection";
-import MeetSection from "./components/MeetSection/MeetSection";
-import OfferSection from "./components/OfferSection/OfferSection";
-import TestimonialSection from "./components/TestimonialSection/TestimonialSection";
-import ContactSection from "./components/ContactSection/ContactSection";
-import Footer from "./components/Footer/Footer";
+// import Header from "./components/Header/Header";
+// import MainSection from "./components/MainSection/MainSection";
+// import MeetSection from "./components/MeetSection/MeetSection";
+// import OfferSection from "./components/OfferSection/OfferSection";
+// import TestimonialSection from "./components/TestimonialSection/TestimonialSection";
+// import ContactSection from "./components/ContactSection/ContactSection";
+// import Footer from "./components/Footer/Footer";
 import ScrollPage from "./components/ScrollPage/ScrollPage";
 
 function App() {
