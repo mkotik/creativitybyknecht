36bbe819ad6376fea7f58801a60120d381a73b16
Marat Kotik <maratkotik97@gmail.com>
2024-11-01 14:10:04 -0400

    create simple scroll snap

--- src/App.js
+++ src/App.js
@@ -1,37 +1,15 @@
-import { Box } from "@mantine/core";
+// import { Box } from "@mantine/core";
 import "./App.scss";
-
-// import Header from "./components/Header/Header";
-// import MainSection from "./components/MainSection/MainSection";
-// import MeetSection from "./components/MeetSection/MeetSection";
-// import OfferSection from "./components/OfferSection/OfferSection";
-// import TestimonialSection from "./components/TestimonialSection/TestimonialSection";
-// import ContactSection from "./components/ContactSection/ContactSection";
-// import Footer from "./components/Footer/Footer";
-import ScrollPage from "./components/ScrollPage/ScrollPage";
 
 function App() {
   return (
-    <Box>
-      <ScrollPage />
-      {/* <Header />
-      <div id="home">
-        <MainSection />
-      </div>
-      <div id="about">
-        <MeetSection />
-      </div>
-      <div id="services">
-        <OfferSection />
-      </div>
-      <div id="testimonials">
-        <TestimonialSection />
-      </div>
-      <div id="contact">
-        <ContactSection />
-      </div>
-      <Footer /> */}
-    </Box>
+    <div className="scroll-container">
+      <div className="scroll-section section-1"></div>
+      <div className="scroll-section section-2"></div>
+      <div className="scroll-section section-3"></div>
+      <div className="scroll-section section-4"></div>
+      <div className="scroll-section section-5"></div>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { MantineProvider, createTheme } from "@mantine/core";
-import "@mantine/core/styles.css";
+// import "@mantine/core/styles.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -36,7 +36,7 @@ const theme = createTheme({
   },
 });
 root.render(
-  <MantineProvider theme={theme}>
-    <App />
-  </MantineProvider>
+  // <MantineProvider theme={theme}>
+  <App />
+  // </MantineProvider>
 );
